f23d46799a7e4c5545f9aa9b2469f4ff20feb6b3
jarvis <kuldipsinh>
2021-04-06 19:22:35 +0530

    Coding Standard checked and tested

--- app/code/Custom/SubscriptionFrequency/Block/Index.php
+++ app/code/Custom/SubscriptionFrequency/Block/Index.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Custom\SubscriptionFrequency\Block;
+
 class Index extends \Magento\Framework\View\Element\Template
 {
 
--- app/code/Custom/SubscriptionFrequency/Controller/Index/Index.php
+++ app/code/Custom/SubscriptionFrequency/Controller/Index/Index.php
@@ -3,6 +3,7 @@
 namespace Custom\SubscriptionFrequency\Controller\Index;
 
 use Custom\SubscriptionFrequency\Model\SubscriptionFrequencyFactory;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Model\Product;
 use Magento\Customer\Api\CustomerRepositoryInterface;
 use Magento\Customer\Model\CustomerFactory;
@@ -13,6 +14,7 @@ use Magento\Framework\View\Result\PageFactory;
 use Magento\Quote\Model\QuoteFactory;
 use Magento\Quote\Model\QuoteManagement;
 use Magento\Sales\Model\Order;
+use Magento\Sales\Model\Order\Email\Sender\OrderSender;
 use Magento\Sales\Model\ResourceModel\Order\CollectionFactory;
 use Magento\Sales\Model\Service\OrderService;
 use Magento\Store\Model\StoreManagerInterface;
@@ -33,7 +35,7 @@ class Index extends Action
     protected $orderSender;
     protected $subscriptionFrequencyFactory;
     /**
-     * @var \Magento\Catalog\Api\ProductRepositoryInterface
+     * @var ProductRepositoryInterface
      */
     protected $productRepository;
 
@@ -50,11 +52,10 @@ class Index extends Action
         CustomerFactory $customerFactory,
         CustomerRepositoryInterface $customerRepository,
         OrderService $orderService,
-        \Magento\Sales\Model\Order\Email\Sender\OrderSender $orderSender,
-        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
+        OrderSender $orderSender,
+        ProductRepositoryInterface $productRepository,
         SubscriptionFrequencyFactory $subscriptionFrequencyFactory
-    )
-    {
+    ) {
         $this->_pageFactory = $pageFactory;
         $this->_orderCollectionFactory = $orderCollectionFactory;
         $this->quote = $quote;
@@ -71,19 +72,17 @@ class Index extends Action
         $this->subscriptionFrequencyFactory = $subscriptionFrequencyFactory;
         return parent::__construct($context);
     }
-
+    // phpcs:ignoreFile
     public function execute()
     {
         $discount = 10;
+
+        $today = date('Y-m-d');
+        $today = date("Y-m-d", strtotime("+1 day", strtotime($today)));
         $collection = $this->_orderCollectionFactory->create()
-            ->addAttributeToSelect('*');
+            ->addAttributeToSelect('*')->addFieldToFilter('created_at', ['to' => $today]);
         $finalArray = [];
 
-        // foreach ($collection->getData() as $key => $value) {
-        //   echo $value['entity_id'] . '<br>';
-        // }
-        // print_r($collection->getData());
-        // exit;
         foreach ($collection->getData() as $key => $value) {
             $order = $this->_order->load($value['entity_id']);
             $orderAllItems = $order->getAllItems();
@@ -95,41 +94,9 @@ class Index extends Action
                 $frequency_options[$value->getSubscriptionfrequencyId()] = $value->getFrequencyName();
             }
 
-//             // if ($order->getStatus() === "complete") {
-// foreach ($orderAllItems as $item) {
-//   if($item->getProductType() == "simple"){
-//     $options = $item->getProductOptions();
-//     if (isset($options['info_buyRequest']['subscription'])) {
-//       $created_date = $item->getCreatedAt();
-//       $today = date('Y-m-d');
-//   //                        echo '<br>';
-//       $today = date("Y-m-d", strtotime("+1 day", strtotime($today)));
-//       while (strtotime($created_date) <= strtotime($today)) {
-//           $dateArray[] = date('Y-m-d', strtotime($created_date));
-//           if ($frequency_options[$options['info_buyRequest']['subscription']] == "Daily") {
-//               $created_date = date("Y-m-d", strtotime("+1 day", strtotime($created_date)));
-//           } elseif ($frequency_options[$options['info_buyRequest']['subscription']] == "Weekly") {
-//               $created_date = date("Y-m-d", strtotime("+1 week", strtotime($created_date)));
-//           } elseif ($frequency_options[$options['info_buyRequest']['subscription']] == "Monthly") {
-//               $created_date = date("Y-m-d", strtotime("+1 month", strtotime($created_date)));
-//           } elseif ($frequency_options[$options['info_buyRequest']['subscription']] == "Yearly") {
-//               $created_date = date("Y-m-d", strtotime("+1 year", strtotime($created_date)));
-//           }
-//       }
-//
-//       echo $item->getProduct()->getName() . '<br>';
-//       echo $item->getProductId() . '<br>';
-//       echo $item->getProductType() . '<br>';
-//     }
-//   }
-// }
-//
-// exit;
-
-                foreach ($orderAllItems as $item) {
-
-                    if($item->getProductType() == "simple"){
-                    $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/testnew.log');
+            foreach ($orderAllItems as $item) {
+                if ($item->getProductType() == "simple") {
+                    $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/cronorders.log');
                     $logger = new \Zend\Log\Logger();
                     $logger->addWriter($writer);
 
@@ -138,38 +105,60 @@ class Index extends Action
                         $created_date = $item->getCreatedAt();
                         $today = date('Y-m-d');
 //                        echo '<br>';
-                        $today = date("Y-m-d", strtotime("+1 day", strtotime($today)));
+                        // $created_date = date("Y-m-d", strtotime("-1 day", strtotime($created_date)));
+                        // echo $today;
+                        $dateArray = [];
                         while (strtotime($created_date) <= strtotime($today)) {
                             $dateArray[] = date('Y-m-d', strtotime($created_date));
                             if ($frequency_options[$options['info_buyRequest']['subscription']] == "Daily") {
-                                $created_date = date("Y-m-d", strtotime("+1 day", strtotime($created_date)));
+                                $created_date = date(
+                                    "Y-m-d",
+                                    strtotime("+1 day", strtotime($created_date))
+                                );
                             } elseif ($frequency_options[$options['info_buyRequest']['subscription']] == "Weekly") {
-                                $created_date = date("Y-m-d", strtotime("+1 week", strtotime($created_date)));
+                                $created_date = date(
+                                    "Y-m-d",
+                                    strtotime("+1 week", strtotime($created_date))
+                                );
                             } elseif ($frequency_options[$options['info_buyRequest']['subscription']] == "Monthly") {
-                                $created_date = date("Y-m-d", strtotime("+1 month", strtotime($created_date)));
+                                $created_date = date(
+                                    "Y-m-d",
+                                    strtotime("+1 month", strtotime($created_date))
+                                );
                             } elseif ($frequency_options[$options['info_buyRequest']['subscription']] == "Yearly") {
-                                $created_date = date("Y-m-d", strtotime("+1 year", strtotime($created_date)));
+                                $created_date = date(
+                                    "Y-m-d",
+                                    strtotime("+1 year", strtotime($created_date))
+                                );
                             }
                         }
 
+                        print_r($dateArray);
+                        // exit;
                         if (in_array($today, $dateArray)) {
-                            $logger->info('in_array ' . $today);
-
-
-                            $finalArray[$order->getEntityId()]['customer_id'] = $order->getCustomerId();
+                            $finalArray[$order->getEntityId()]['customer_id']
+                                = $order->getCustomerId();
                             $finalArray[$order->getEntityId()]['email'] = $order->getCustomerEmail();
-
-//                            $finalArray[$order->getEntityId()]['shipping_address'] = $order->getShippingAddress();
-                            $finalArray[$order->getEntityId()]['shipping_address']['firstname'] = $order->getShippingAddress()->getFirstname();
-                            $finalArray[$order->getEntityId()]['shipping_address']['lastname'] = $order->getShippingAddress()->getLastname();
-                            $finalArray[$order->getEntityId()]['shipping_address']['city'] = $order->getShippingAddress()->getCity();
-                            $finalArray[$order->getEntityId()]['shipping_address']['country_id'] = $order->getShippingAddress()->getCountryId();
-                            $finalArray[$order->getEntityId()]['shipping_address']['street'] = $order->getShippingAddress()->getStreet();
-                            $finalArray[$order->getEntityId()]['shipping_address']['region'] = $order->getShippingAddress()->getRegion();
-                            $finalArray[$order->getEntityId()]['shipping_address']['region_id'] = $order->getShippingAddress()->getRegionId();
-                            $finalArray[$order->getEntityId()]['shipping_address']['postcode'] = $order->getShippingAddress()->getPostcode();
-                            $finalArray[$order->getEntityId()]['shipping_address']['telephone'] = $order->getShippingAddress()->getTelephone();
-                            $finalArray[$order->getEntityId()]['shipping_address']['fax'] = $order->getShippingAddress()->getFax();
+                            $finalArray[$order->getEntityId()]['shipping_address']['firstname']
+                                = $order->getShippingAddress()->getFirstname();
+                            $finalArray[$order->getEntityId()]['shipping_address']['lastname']
+                                = $order->getShippingAddress()->getLastname();
+                            $finalArray[$order->getEntityId()]['shipping_address']['city']
+                                = $order->getShippingAddress()->getCity();
+                            $finalArray[$order->getEntityId()]['shipping_address']['country_id']
+                                = $order->getShippingAddress()->getCountryId();
+                            $finalArray[$order->getEntityId()]['shipping_address']['street']
+                                = $order->getShippingAddress()->getStreet();
+                            $finalArray[$order->getEntityId()]['shipping_address']['region']
+                                = $order->getShippingAddress()->getRegion();
+                            $finalArray[$order->getEntityId()]['shipping_address']['region_id']
+                                = $order->getShippingAddress()->getRegionId();
+                            $finalArray[$order->getEntityId()]['shipping_address']['postcode']
+                                = $order->getShippingAddress()->getPostcode();
+                            $finalArray[$order->getEntityId()]['shipping_address']['telephone']
+                                = $order->getShippingAddress()->getTelephone();
+                            $finalArray[$order->getEntityId()]['shipping_address']['fax']
+                                = $order->getShippingAddress()->getFax();
                             $finalArray[$order->getEntityId()]['billing_address'] = $order->getBillingAddress();
                             $finalArray[$order->getEntityId()]['items'] = $order->getItems();
                             $finalArray[$order->getEntityId()]['customer_name'] = $order->getCustomerName();
@@ -192,33 +181,17 @@ class Index extends Action
                             } else {
                                 $finalArray[$order->getEntityId()]['special_price_discount'] = 0;
                             }
-                            $finalArray[$order->getEntityId()]['special_price'] = $item->getProduct()->getSpecialPrice();
+                            $finalArray[$order->getEntityId()]['special_price']
+                                = $item->getProduct()->getSpecialPrice();
                             $finalArray[$order->getEntityId()]['sku'] = $item->getSku();
                             $finalArray[$order->getEntityId()]['order_date'] = $item->getCreatedAt();
-                            $finalArray[$order->getEntityId()]['subscription_type'] = $options['info_buyRequest']['subscription_type'];
-                            $finalArray[$order->getEntityId()]['subscription'] = $options['info_buyRequest']['subscription'];
+                            $finalArray[$order->getEntityId()]['subscription_type']
+                                = $options['info_buyRequest']['subscription_type'];
+                            $finalArray[$order->getEntityId()]['subscription']
+                                = $options['info_buyRequest']['subscription'];
                             $finalArray[$order->getEntityId()]['subscription_start'] = $item->getCreatedAt();
                             $finalArray[$order->getEntityId()]['discount'] = $discount;
                             $finalArray[$order->getEntityId()]['created_at'] = $today . date('H:i:s');
-                            $model = $this->_objectManager->create('Custom\SubscriptionsOrders\Model\SubscriptionsOrders');
-                            $model->load('1');
-
-                            $today_start = $today . " 00:00:01";
-                            $today_end = $today . " 23:59:59";
-
-
-                           $schoolCollection = $this->_orderCollectionFactory->create()->getCollection()->addFieldToFilter('order_id', $order->getEntityId())->addFieldToFilter('created_at', ['from' => $today_start, 'to' => $today_end]);
-                           if ($schoolCollection->getSize() > 0) {
-                             echo 'if';
-                           } else {
-                             echo 'else';
-//                                print_r($finalArray);
-//                                exit();
-//                                $model->setData($finalArray[$order->getEntityId()]);
-//                                $model->save();
-                          }
-                          exit;
-                            /*asdad*/
                             $orderInfo = [
                                 'email' => $finalArray[$order->getEntityId()]['email'], //customer email id
                                 'currency_id' => 'USD',
@@ -235,7 +208,7 @@ class Index extends Action
                                     'postcode' => $finalArray[$order->getEntityId()]['shipping_address']['postcode'],
                                     'telephone' => $finalArray[$order->getEntityId()]['shipping_address']['telephone'],
                                     'fax' => $finalArray[$order->getEntityId()]['shipping_address']['fax'],
-                                    'save_in_address_book' => 1
+                                    'save_in_address_book' => 0
                                 ],
                                 'items' => $finalArray[$order->getEntityId()]['items']];
                             $store = $this->storeManager->getStore();
@@ -290,8 +263,10 @@ class Index extends Action
                             $shippingAddress = $quote->getShippingAddress();
                             $shippingAddress->setCollectShippingRates(true)
                                 ->collectShippingRates()
-                                ->setShippingMethod('flatrate_flatrate'); //shipping method code, Make sure free shipping method is enabled
-                            $quote->setPaymentMethod('cashondelivery'); //Payment Method Code, Make sure checkmo payment method is enabled
+                                ->setShippingMethod('flatrate_flatrate');
+                            //shipping method code, Make sure free shipping method is enabled
+                            $quote->setPaymentMethod('cashondelivery');
+                            //Payment Method Code, Make sure checkmo payment method is enabled
                             $quote->setInventoryProcessed(false);
                             $quote->save();
                             $quote->getPayment()->importData(['method' => 'cashondelivery']);
@@ -305,18 +280,15 @@ class Index extends Action
                             // Get Order Increment ID
                             $orderId = $order->getIncrementId();
                             if ($orderId) {
-                                $result['success'] = $orderId;
+                                // $result['success'] = $orderId;
+                                $logger->info('Order Placed ' . $orderId);
                             } else {
-                                $result = ['error' => true, 'msg' => 'Error occurs for Order placed'];
+                                $logger->info("Error occurs for Order placed");
                             }
-                            print_r($result);
-                            /*asdad*/
-//                            }
                         }
                     }
-                  }
                 }
-            // }
+            }
         }
     }
 }
